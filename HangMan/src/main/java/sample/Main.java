e9cff47e7e9d2c48e4cb1afd181cb852f0014037
patryk.popek <patrykpopek16@gmail.com>
2024-02-02 11:58:59 +0100

    Stworzenie sceny dla menu bazy wyrazów

--- HangMan/src/main/java/sample/Main.java
+++ HangMan/src/main/java/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,11 +10,25 @@ import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+    public void start(Stage stage) throws IOException
+    {
+        FXMLLoader mainWindowLoader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+        Parent mainWindow = mainWindowLoader.load();
+        MainWindowController mainWindowController = mainWindowLoader.getController();
+        mainWindowController.setStage(stage);
+
+        FXMLLoader dbWindowLoader = new FXMLLoader(getClass().getResource("MainWordDB.fxml"));
+        Parent dbWindow = dbWindowLoader.load();
+        MainWordDBController dbWindowController = dbWindowLoader.getController();
+        dbWindowController.setStage(stage);
+
+        Scene mainScene=new Scene(mainWindow, 800, 600);
+        mainWindowController.setDbScene(new Scene(dbWindow, 800, 600));
+        dbWindowController.setmainScene(mainScene);
+
+
         stage.setTitle("HangMan Game");
-        stage.setScene(scene);
+        stage.setScene(mainScene);
         stage.show();
     }
 
--- HangMan/src/main/java/sample/MainWindowController.java
+++ HangMan/src/main/java/sample/MainWindowController.java
@@ -1,14 +1,34 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class MainWindowController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class MainWindowController implements Initializable {
+
+    private Stage mainStage;
+    private Scene dbScene;
+
+    public void setStage(Stage stage)
+    {
+        this.mainStage=stage;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    public void setDbScene(Scene scene) {
+        this.dbScene = scene;
+    }
+    public void showDBScene()
+    {
+        mainStage.setScene(dbScene);
     }
 }
