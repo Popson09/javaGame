3cd59ae07785b2dfe6d9d6f71dd0b519be367fc8
Popsonikk <popson2001@onet.pl>
2024-02-13 12:46:02 +0100

    Stworzenie sceny dla menu bazy wyrazów

---
+++ HangMan/src/main/java/sample/MainWordDBController.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWordDBController implements Initializable {
+    private Stage dbStage;
+    private Scene mainScene;
+
+    public void setStage(Stage stage)
+    {
+        this.dbStage=stage;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    public void setmainScene(Scene scene) {
+        this.mainScene = scene;
+    }
+
+    public void showMainScene() {
+        dbStage.setScene(mainScene);
+    }
+}
