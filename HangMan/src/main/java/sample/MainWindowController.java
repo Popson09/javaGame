7c293f8a81d8b93bf2bafad9fbbb295334827951
Popsonikk <popson2001@onet.pl>
2024-02-13 12:45:30 +0100

    Rename basic classes

--- HangMan/src/main/java/sample/MainWindowController.java
+++ HangMan/src/main/java/sample/MainWindowController.java
@@ -3,7 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainWindowController {
     @FXML
     private Label welcomeText;
 
